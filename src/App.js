835dce5b3118bafa7b6f20b8487df74879361a4b
Chaminu <trananhprince@gmail.com>
2022-11-28 18:07:19 +0700

    init Firebase analytics

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import AppRouter from './router';
+import ReactGA from 'react-ga4';
 import 'animate.css';
 
 function App() {
     document.documentElement.style.setProperty('--animate-duration', '.75s');
+
+    ReactGA.initialize('G-N36MTPPYR4');
+    ReactGA.send('pageview');
 
     return (
         <div className="bg-[#f2f2f2] overflow-x-hidden" draggable={false}>
